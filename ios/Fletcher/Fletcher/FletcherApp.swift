4f0edcae4b6c6fb6a89e3c5364bb334dc1b56af3
Dennis Yang <sinned@gmail.com>
2025-12-15 22:56:41 -0800

    fix: Migrate stored serverURL from localhost to Render

--- ios/Fletcher/Fletcher/FletcherApp.swift
+++ ios/Fletcher/Fletcher/FletcherApp.swift
@@ -4,6 +4,14 @@ import SwiftUI
 struct FletcherApp: App {
     @StateObject private var locationStore = LocationStore.shared
     @StateObject private var locationService = BackgroundLocationService()
+
+    init() {
+        // Migration: If serverURL is stuck on localhost default, update it to Render
+        let stored = UserDefaults.standard.string(forKey: "serverURL")
+        if stored == "http://localhost:3000" {
+            UserDefaults.standard.set("https://fletcher-server.onrender.com", forKey: "serverURL")
+        }
+    }
     
     var body: some Scene {
         WindowGroup {
