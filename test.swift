d2de8a29738116b5abbc20a2cfe80c3695e273b9
Dennis Yang <sinned@gmail.com>
2026-02-21 19:51:21 -0800

    chore: remove test files

--- test.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-UserDefaults.standard.set(30, forKey: "testKey")
-let obj = UserDefaults.standard.object(forKey: "testKey")
-print("Obj: \(String(describing: obj))")
-let casted = obj as? Int
-print("Casted: \(String(describing: casted))")
